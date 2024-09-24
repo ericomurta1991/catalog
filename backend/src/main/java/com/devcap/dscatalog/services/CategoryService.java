865a98c92b94f1c82f71fc71dbeb74c380ce44d6
ericomurta1991 <erico_murta@yahoo.com.br>
2024-09-24 09:48:43 -0300

    Find By Id

--- backend/src/main/java/com/devcap/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devcap/dscatalog/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.devcap.dscatalog.services;
 
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,36 @@ public class CategoryService {
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	
 	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	/* FOR
 	List<CategoryDTO> listDto = new ArrayList<>();
