45af1426e7fef2d438ee43afbf221eb86f00cc00
ericomurta1991 <erico_murta@yahoo.com.br>
2024-09-24 09:17:20 -0300

    DTO

--- backend/src/main/java/com/devcap/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devcap/dscatalog/resources/CategoryResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devcap.dscatalog.entities.Category;
+import com.devcap.dscatalog.dto.CategoryDTO;
 import com.devcap.dscatalog.services.CategoryService;
 
 @RestController
@@ -20,13 +20,13 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
+	public ResponseEntity<List<CategoryDTO>> findAll(){
 	/*	List<Category> list = new ArrayList<>();
 		list.add(new Category(1L, "Books"));
 		list.add(new Category(2L, "Eletronics"));
 		return ResponseEntity.ok().body(list);
 	*/
-		List<Category> list = service.findAll();
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/devcap/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devcap/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.devcap.dscatalog.services;
 
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devcap.dscatalog.dto.CategoryDTO;
 import com.devcap.dscatalog.entities.Category;
 import com.devcap.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +19,25 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	
+	}
+	
+	/* FOR
+	List<CategoryDTO> listDto = new ArrayList<>();
+	for(Category cat : list) {
+		listDto.add(new CategoryDTO(cat));
+	}
+	return listDto;
+	
+	*/
+	
+	
+	/*  //NO DTO
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
-	}
+	*/
 }
